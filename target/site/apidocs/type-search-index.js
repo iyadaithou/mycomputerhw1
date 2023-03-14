389c492deb4a0f83f1e48603e9fef81942a3971f
iyadaithou <66526110+iyadaithou@users.noreply.github.com>
2023-03-14 09:56:30 -0400

    a

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.teama.computer","l":"Computer"},{"p":"com.teama.computer","l":"Simulator"}];updateSearchResults();
